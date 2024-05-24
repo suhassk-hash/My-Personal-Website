402b15417bb29f5d4b7061598a886344545c1a4e
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-05-24 20:19:14 +0530

    NAv bar using js

--- script/main.js
+++ script/main.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
     const navBarIcon = document.querySelector(".Nav-bar-icon");
     const navBarR = document.querySelector(".Nav-bar-r");
-
-    navBarIcon.addEventListener("click", function() {
-        if (navBarR.style.display === "block") {
-            navBarR.style.display = "none";
-        } else {
-            navBarR.style.display = "block";
-        }
-    });
-});
+     navBarIcon.onclick=responsive;
+        function responsive() {
+            if (navBarR.style.display === "block") {
+                navBarR.style.display = "none";
+                navBarIcon.style.marginTop = "0";
+            } else {
+                navBarIcon.style.marginTop = "-25%";
+                navBarIcon.style.right = "0px";
+                navBarR.style.display = "block";
+                navBarR.style.opacity = "100%";
+                navBarR.style.width = "90%";
+                navBarR.style.marginTop = "25%"; 
+                navBarR.style.padding = "2% 2% 2% 4%";
+                navBarR.style.color = "#e6f3ff";
+                navBarR.style.boxShadow = "0 2px 4px rgba(194, 183, 183, 0.808)";
+            }
+        };    
