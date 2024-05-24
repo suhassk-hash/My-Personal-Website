8393de9ff4869244ea462b825bfc8932207a0256
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-05-24 20:31:50 +0530

    commit

--- script/main.js
+++ script/main.js
@@ -13,7 +13,7 @@ function responsive() {
         navBarR.style.opacity = "100%";
         navBarR.style.width = "100%";
         navBarR.style.marginTop = "30%";
-        navBarR.style.padding = "2rem 2rem 2rem 4rem";
+        navBarR.style.padding = "2% 2% 2% 4%";
         navBarR.style.color = "#e6f3ff";
         navBarR.style.boxShadow = "0 2px 4px rgba(194, 183, 183, 0.808)";
     }
