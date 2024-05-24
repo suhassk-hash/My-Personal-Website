b5a0a45fc2c45024c476785408ba5c0e01026c07
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-05-24 20:28:16 +0530

    nav bar shift removed

--- script/main.js
+++ script/main.js
@@ -1,19 +1,20 @@
-    const navBarIcon = document.querySelector(".Nav-bar-icon");
-    const navBarR = document.querySelector(".Nav-bar-r");
-     navBarIcon.onclick=responsive;
-        function responsive() {
-            if (navBarR.style.display === "block") {
-                navBarR.style.display = "none";
-                navBarIcon.style.marginTop = "0";
-            } else {
-                navBarIcon.style.marginTop = "-25%";
-                navBarIcon.style.right = "0px";
-                navBarR.style.display = "block";
-                navBarR.style.opacity = "100%";
-                navBarR.style.width = "90%";
-                navBarR.style.marginTop = "25%"; 
-                navBarR.style.padding = "2% 2% 2% 4%";
-                navBarR.style.color = "#e6f3ff";
-                navBarR.style.boxShadow = "0 2px 4px rgba(194, 183, 183, 0.808)";
-            }
-        };    
+const navBarIcon = document.querySelector(".Nav-bar-icon");
+const navBarR = document.querySelector(".Nav-bar-r");
+navBarIcon.style.position = "fixed"; 
+navBarIcon.style.top = "0"; 
+navBarIcon.style.right = "0";
+navBarIcon.onclick = responsive;
+
+function responsive() {
+    if (navBarR.style.display === "block") {
+        navBarR.style.display = "none";
+    } else {
+        navBarR.style.display = "block";
+        navBarR.style.opacity = "100%";
+        navBarR.style.width = "100%";
+        navBarR.style.marginTop = "30%";
+        navBarR.style.padding = "2rem 2rem 2rem 4rem";
+        navBarR.style.color = "#e6f3ff";
+        navBarR.style.boxShadow = "0 2px 4px rgba(194, 183, 183, 0.808)";
+    }
+};
