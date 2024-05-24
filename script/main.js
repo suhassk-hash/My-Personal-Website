5901dc4a9480e44e5fc4bfe399233206e8d9855e
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-05-24 23:13:59 +0530

    perfect responsive

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ const navBarIcon = document.querySelector(".Nav-bar-icon");
 const navBarR = document.querySelector(".Nav-bar-r");
 navBarIcon.style.position = "fixed"; 
 navBarIcon.style.top = "0"; 
-navBarIcon.style.right = "0";
+navBarIcon.style.right = "6%";
 navBarIcon.onclick = responsive;
 
 function responsive() {
@@ -10,10 +10,9 @@ function responsive() {
         navBarR.style.display = "none";
     } else {
         navBarR.style.display = "block";
-        navBarR.style.opacity = "100%";
+        navBarR.style.opacity = "1";
         navBarR.style.width = "90%";
         navBarR.style.marginTop = "40%";
-        navBarR.style.padding = "2% 2% 2% 4%";
         navBarR.style.color = "#e6f3ff";
     }
 };
