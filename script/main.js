d266f755cae9f5d7eff66665faed7aade229e1e2
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-05-29 01:30:58 +0530

    changed navbar

--- script/main.js
+++ script/main.js
@@ -1,18 +1,17 @@
 const navBarIcon = document.querySelector(".Nav-bar-icon");
-const navBarR = document.querySelector(".Nav-bar-r");
+const navBarR = document.querySelector(".Nav-bar");
+const windowSize=document.documentElement.clientWidth;
 navBarIcon.style.position = "fixed"; 
 navBarIcon.style.top = "0"; 
 navBarIcon.style.right = "6%";
 navBarIcon.onclick = responsive;
-
 function responsive() {
-    if (navBarR.style.display === "block") {
+    if (navBarR.style.display === "flex") {
         navBarR.style.display = "none";
     } else {
-        navBarR.style.display = "block";
+        navBarR.style.display = "flex";
         navBarR.style.opacity = "1";
         navBarR.style.width = "90%";
-        navBarR.style.marginTop = "10%";
         navBarR.style.color = "#e6f3ff";
     }
 };
